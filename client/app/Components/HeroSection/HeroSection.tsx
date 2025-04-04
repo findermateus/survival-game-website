5a0071c2c8ec909978bf91d7475963169694c601
findermateus <mateusfinder@gmail.com>
2025-04-04 19:27:28 -0300

    feat: hero section

---
+++ client/app/Components/HeroSection/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SteamButton from "~/Components/HeroSection/SteamButton";
+
+
+
+const HeroSection: React.FC = () => {
+    return <section>
+        <SteamButton />
+    </section>
+}
+
+export default HeroSection;
---
+++ client/app/Components/HeroSection/SteamButton/index.tsx
@@ -0,0 +1,17 @@
+import DefaultButton from "~/Components/DefaultButton";
+import "./styles.css";
+import React from "react";
+import logo from "~/Assets/steam_icon_logo.svg";
+
+function getSteamIntegrationButtonContent(): React.ReactNode {
+    return <div className="steam-button-content">
+        <span><img src={logo} alt="Steam" width={60} height={60}/></span>
+        <span>Compre o jogo na steam</span>
+    </div>
+}
+
+const SteamButton: React.FC = () => {
+    return <DefaultButton children={getSteamIntegrationButtonContent()}/>
+}
+
+export default SteamButton;
