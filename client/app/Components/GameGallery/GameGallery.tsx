1223eb02c915c931bf9b8c51aed73e0a2a272dbe
finder <mateusfinder@gmail.com>
2025-04-06 18:23:57 -0300

    feat: componentes de apresentação do jogo

--- client/app/Components/GameGallery/GameGallery.tsx
+++ client/app/Components/GameGallery/GameGallery.tsx
@@ -1,9 +1,14 @@
 import "./styles.css";
 import DefaultTitle from "~/Components/Assets/DefaultTitle";
+import LateralGifRepository from "~/Components/GameGallery/LateralGifRepository";
 
 const GameGallery = () => {
     return <div>
         <DefaultTitle value="Extraction Game" size="60px" />
+        <div className="gallery-container">
+            <LateralGifRepository />
+            <LateralGifRepository />
+        </div>
     </div>
 }
 
